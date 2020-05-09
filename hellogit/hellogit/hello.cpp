cb5fb22986e54953ae91911c858905f21082d751
mengyu <944894437@qq.com>
2020-05-10 00:13:14 +0800

    CommitHellocpp

---
+++ hellogit/hellogit/hello.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hellogit
+//
+//  Created by apple on 20/5/9.
+//  Copyright © 2020年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Git!\n";
+    return 0;
+}
